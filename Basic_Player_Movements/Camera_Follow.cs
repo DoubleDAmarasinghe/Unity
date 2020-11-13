87fcd99b9adf83e6383139ff039c9bfb45005855
Dilanga D Amarasinghe <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2020-11-13 17:59:06 +0530

    Add files via upload

---
+++ Basic_Player_Movements/Camera_Follow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+public Transform robot;
+
+void FixedUpdate()
+{
+	transform.position = new Vector3(robot.position.x, robot.position.y, transform.position.z);
+}
+}
